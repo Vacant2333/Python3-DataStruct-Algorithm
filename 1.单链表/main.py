459fdf71f87422b9eb6df9524ed91b95ffa176e9
Vacant <vacant2333@gmail.com>
2021-04-20 22:35:10 +0800

    单链表

--- 1.单链表/main.py
+++ 1.单链表/main.py
@@ -14,26 +14,30 @@ class SingleLinkList:
 
     def is_empty(self) -> bool:
         #判断链表是否为空
-        return False if self.head.next else True
+        return False if (self.head.next) else True
 
     def length(self) -> int:
         #获取链表长度
         return self.head.data
 
-    def has_key(self, key):
-        #查询是否有此节点
-        return True if key >= self.length() else False
+    def items(self) -> list:
+        re = []
+        cur = self.head.next
+        while(cur != None):
+            re.append(cur.data)
+            cur = cur.next
+        return re
 
     def add_top(self, data) -> bool:
         #向链表头部添加节点
         tmp = self.head.next
         self.head.next = Node(data)
         self.head.next.next = tmp
+        self.head.data += 1
         return True
 
     def add_bottom(self, data) -> bool:
         #向链表尾部添加节点
-        self.head.data += 1
         if(self.head.next == None):
             self.head.next = Node(data)
         else:
@@ -41,4 +45,27 @@ class SingleLinkList:
             while(cur.next != None):
                 cur = cur.next
             cur.next = Node(data)
+        self.head.data += 1
+        return True
+
+    def remove(self, data) -> bool:
+        #删除节点
+        if(self.length() <= 1):
+            if(self.length() == 0):
+                return False
+            else:
+                if(self.head.next.data == data):
+                    self.head.next = None
+                    return True
+                else:
+                    return False
+        cur = self.head.next
+        last_cur = None
+        while(cur.data != data):
+            if(cur.next == None):
+                return False
+            last_cur = cur
+            cur = cur.next
+        last_cur.next = cur.next
+        self.head.data -= 1
         return True
--- 1.单链表/test.py
+++ 1.单链表/test.py
@@ -2,8 +2,10 @@ from main import SingleLinkList
 
 sll = SingleLinkList()
 
-sll.add_top(1)
-sll.add_top(2)
-#sll.add_top(3)
+sll.add_bottom(1)
+sll.add_bottom(2)
+sll.add_bottom(3)
+sll.remove(2)
 print(sll.head.next.next.data)
 print(sll.head.data)
+print(sll.items())
