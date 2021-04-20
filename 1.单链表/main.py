b37e0fad5e1ee912476ce4baea024897dea229c4
Vacant <vacant2333@gmail.com>
2021-04-20 22:44:23 +0800

    单链表

--- 1.单链表/main.py
+++ 1.单链表/main.py
@@ -21,6 +21,7 @@ class SingleLinkList:
         return self.head.data
 
     def items(self) -> list:
+        #获得链表所有数据
         re = []
         cur = self.head.next
         while(cur != None):
@@ -39,6 +40,7 @@ class SingleLinkList:
     def add_bottom(self, data) -> bool:
         #向链表尾部添加节点
         if(self.head.next == None):
+            #判断链表是否为空
             self.head.next = Node(data)
         else:
             cur = self.head.next
@@ -51,21 +53,25 @@ class SingleLinkList:
     def remove(self, data) -> bool:
         #删除节点
         if(self.length() <= 1):
+            #判断链表长度
             if(self.length() == 0):
+                #判断是否是空链表
                 return False
             else:
                 if(self.head.next.data == data):
+                    #判断唯一元素是否是data
                     self.head.next = None
-                    return True
                 else:
                     return False
-        cur = self.head.next
-        last_cur = None
-        while(cur.data != data):
-            if(cur.next == None):
-                return False
-            last_cur = cur
-            cur = cur.next
-        last_cur.next = cur.next
+        else:
+            cur = self.head.next
+            last_cur = None
+            #last_cur : 存放上一个节点
+            while(cur.data != data):
+                if(cur.next == None):
+                    return False
+                last_cur = cur
+                cur = cur.next
+            last_cur.next = cur.next
         self.head.data -= 1
         return True
