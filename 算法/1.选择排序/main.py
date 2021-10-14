c672b51b9356d531bc8d4497b107f645112c4fd9
Vacant <vacant2333@gmail.com>
2021-10-14 22:10:47 +0800

    Selection sort

--- 算法/1.选择排序/main.py
+++ 算法/1.选择排序/main.py
@@ -11,17 +11,13 @@ class SelectionSort:
                         smallest_key = key
                 else:
                     smallest_key = key
-        return -1 if smallest_key is None else smallest_key
+        return smallest_key
 
     def selection_sort(self, arr: list, sort: int = 1) -> list:
         # 选择排序 sort :0升序 1降序
         new_arr, key, smallest_key = [[], None, 0]
         for key in range(len(arr)):
             smallest_key = self.find_smallest_value(arr)
-            print(smallest_key)
-            if smallest_key != -1:
-                new_arr.append(arr[smallest_key])
-                arr[smallest_key] = None
-            else:
-                # return sort == 1 if new_arr.reverse() else new_arr
-                return new_arr if sort == 0 else new_arr.reverse()
+            new_arr.append(arr[smallest_key])
+            arr[smallest_key] = None
+        return new_arr if sort == 0 else new_arr[::-1]
--- 算法/1.选择排序/test.py
+++ 算法/1.选择排序/test.py
@@ -5,4 +5,6 @@ arr = [10, 234, 43, 31, 44, 321, 342, 123, 124, 355, 789, 77, 65, 54, 65]
 
 ss = SelectionSort()
 
-print(ss.selection_sort(arr, 0))
+print(ss.selection_sort(arr, 1))
+
+start_time = int(round(time.time() * 1000))
