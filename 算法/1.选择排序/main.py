f4ea3ecf4e4ca9437f3cc3c97a13a0894c5d4b61
Vacant <vacant2333@gmail.com>
2021-10-14 22:00:12 +0800

    Selection sort

--- 算法/1.选择排序/main.py
+++ 算法/1.选择排序/main.py
@@ -1,3 +1,27 @@
 class SelectionSort:
-    def find_smallest(self, arr):
-        return
+    # 选择排序
+
+    def find_smallest_value(self, arr: list) -> int:
+        # 返回最小值的索引 数组为空则返回-1
+        smallest_key, key = [None, 0]
+        for key in range(len(arr)):
+            if arr[key] is not None:
+                if smallest_key is not None:
+                    if arr[key] < arr[smallest_key]:
+                        smallest_key = key
+                else:
+                    smallest_key = key
+        return -1 if smallest_key is None else smallest_key
+
+    def selection_sort(self, arr: list, sort: int = 1) -> list:
+        # 选择排序 sort :0升序 1降序
+        new_arr, key, smallest_key = [[], None, 0]
+        for key in range(len(arr)):
+            smallest_key = self.find_smallest_value(arr)
+            print(smallest_key)
+            if smallest_key != -1:
+                new_arr.append(arr[smallest_key])
+                arr[smallest_key] = None
+            else:
+                # return sort == 1 if new_arr.reverse() else new_arr
+                return new_arr if sort == 0 else new_arr.reverse()
--- 算法/1.选择排序/test.py
+++ 算法/1.选择排序/test.py
@@ -1,2 +1,8 @@
+import time
+from main import SelectionSort
+
 arr = [10, 234, 43, 31, 44, 321, 342, 123, 124, 355, 789, 77, 65, 54, 65]
 
+ss = SelectionSort()
+
+print(ss.selection_sort(arr, 0))
