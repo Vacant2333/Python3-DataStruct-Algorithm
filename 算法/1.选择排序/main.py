b3fe5e40f2e04932f7c5e60c109a54e4207fb841
Vacant <vacant2333@gmail.com>
2021-10-15 12:38:49 +0800

    selection sort

--- 算法/1.选择排序/main.py
+++ 算法/1.选择排序/main.py
@@ -22,3 +22,25 @@ class SelectionSort:
             new_arr.append(arr[smallest_key])
             arr[smallest_key] = None
         return new_arr if sort == 0 else new_arr[::-1]
+
+# 优化后
+class SelectionSort2:
+    # 选择排序
+
+    def find_smallest_value(self, arr: list) -> int:
+        # 返回最小值的索引 数组为空则返回-1
+        min_value: int = 999999
+        min_value_index: int = 0
+        for index, value in enumerate(arr):
+            if value < min_value:
+                min_value_index = index
+                min_value = value
+        return arr.pop(min_value_index)
+
+    def selection_sort(self, ar: list, sort: int = 1) -> list:
+        # 选择排序 sort :0升序 1降序
+        arr = ar[:]
+        new_arr, key, smallest_key = [[], None, 0]
+        for key in range(len(arr)):
+            new_arr.append(self.find_smallest_value(arr))
+        return new_arr if sort == 0 else new_arr[::-1]
--- 算法/1.选择排序/test.py
+++ 算法/1.选择排序/test.py
@@ -1,19 +1,26 @@
 import random
 import time
-from main import SelectionSort
-
+from main import SelectionSort, SelectionSort2
 ss = SelectionSort()
+ss2 = SelectionSort2()
 arr = []
 
 start_time = int(round(time.time() * 1000))
 
-for _ in range(100000):
+for _ in range(10):
     arr.append(random.randint(1, 100000))
 
 ss_result = ss.selection_sort(arr)
 ss_time = int(round(time.time() * 1000))
 print("选择排序耗时:{}毫秒".format(ss_time - start_time))
+
 py_result = sorted(arr, reverse=True)
 py_time = int(round(time.time() * 1000))
 print("py sorted排序耗时:{}毫秒".format(py_time - ss_time))
+
+ss2_result = ss2.selection_sort(arr)
+ss2_time = int(round(time.time() * 1000))
+print("选择排序2耗时:{}毫秒".format(ss2_time - py_time))
+
 print(py_result == ss_result)
+print(py_result == ss2_result)
