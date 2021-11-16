767d957b1a9943d9545b309b33bae15b8e065141
Vacant <vacant2333@gmail.com>
2021-11-16 22:15:33 +0800

    graph

--- 图/武汉地铁最短路径/spider.py
+++ 图/武汉地铁最短路径/spider.py
@@ -1,24 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+# 爬取武汉所有地铁的线路信息
 
 url = 'http://wh.bendibao.com/ditie/linemap.shtml'
 soup = BeautifulSoup(requests.get(url).text, 'html.parser')
 
-# 线路数据(所有站点)
-line_data = {
-    '1': [],
-    '2': [],
-    '3': [],
-    '4': [],
-    '6': [],
-    '7': [],
-    '8': [],
-    '阳逻': [],
-    '11': []
-}
+# 线路数据
+line_data = {}
 
-for station_list in soup.find_all("div", class_="line-list-station"):
-    print(len(station_list))
+for line_div in soup.find_all("div", class_="line-list"):
+    # heading div 用于获得station name
+    heading_div = line_div.findChildren(class_="line-list-heading")
+    wrap_div = heading_div[0].findChildren(class_="wrap")
+    # 站点名,去掉武汉 和 线路图五个字
+    station_name = wrap_div[0].strong.a.contents[0][2:-3]
+    # 所有站点
+    stations_div = line_div.findChildren(class_="line-list-station")[0].findChildren(class_="station")
+    line_data[station_name] = []
+    # 把站点名全部加到数据中
+    for station_div in stations_div:
+        line_data[station_name].append(station_div.a.contents)
 
-
-
+print(line_data)
