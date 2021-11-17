612e90b69f3546aeaeea4dc4a3d907813fc00f57
Vacant <vacant2333@gmail.com>
2021-11-17 19:59:11 +0800

    graph

--- 图/武汉地铁最短路径/graph.py
+++ 图/武汉地铁最短路径/graph.py
@@ -1,22 +1,32 @@
 import spider
+import networkx as nx
+import matplotlib.pyplot as plt
 
 line_data = spider.get_line_data()
-
 # 节点
 nodes = {}
+# 边
 edges = {}
+# 上一个站点的名字
 last_station_name = ''
-
-for (line_name, line) in line_data.items():
+for line in line_data.values():
     for station in line:
-        nodes[station] = {
-            'color': 'green'
-        }
+        # keys存节点(车站)名 value存节点颜色
+        nodes[station] = 'green'
         if last_station_name != '' and last_station_name != station:
-            edges[(last_station_name, station)] = {
-                'color': 'black'
-            }
+            # 连接车站,边 颜色为black
+            edges[(last_station_name, station)] = 'black'
         last_station_name = station
-
-print(nodes)
-print(edges)
+    last_station_name = ''
+# 默认Graph
+graph = nx.Graph()
+# 添加节点和边到graph中
+graph.add_nodes_from(nodes)
+graph.add_edges_from(edges.keys())
+# 支持中文
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+# 画布大小8000*4000
+plt.figure(figsize=(80, 40))
+nx.draw(graph, with_labels=True, node_size=[750], font_size=50, width=6, node_color=nodes.values(), edge_color=edges.values())
+plt.show()
--- 图/武汉地铁最短路径/spider.py
+++ 图/武汉地铁最短路径/spider.py
@@ -14,11 +14,14 @@ def get_line_data():
         heading_div = line_div.findChildren(class_="line-list-heading")
         wrap_div = heading_div[0].findChildren(class_="wrap")
         # 站点名,去掉武汉 和 线路图五个字
-        station_name = wrap_div[0].strong.a.contents[0][2:-3]
+        line_name = wrap_div[0].strong.a.contents[0][2:-3]
         # 所有站点
-        stations_div = line_div.findChildren(class_="line-list-station")[0].findChildren(class_="station")
-        line_data[station_name] = []
+        all_station_div = line_div.findChildren(class_="line-list-station")[0].findChildren(class_="station")
+        # 初始化这条线的数据
+        line_data[line_name] = []
         # 把站点名全部加到数据中
-        for station_div in stations_div:
-            line_data[station_name].append(station_div.a.contents[0])
+        for station_div in all_station_div:
+            # 根据class为link 拿到所有a标签(站点)
+            real_station_div = station_div.findChildren(class_="link")[0]
+            line_data[line_name].append(real_station_div.contents[0])
     return line_data
