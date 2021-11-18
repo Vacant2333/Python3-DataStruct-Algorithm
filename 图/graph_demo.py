c0361d9dac5921433cb51ce8f722b2f180887388
Vacant <vacant2333@gmail.com>
2021-11-18 20:50:31 +0800

    readme

--- 图/graph_demo.py
+++ 图/graph_demo.py
@@ -19,19 +19,15 @@ edges = [
     ('b', 'c', 4)
 ]
 
-# new a graph
+# 新graph
 graph = nx.Graph()
-# add nodes and edges with weight
+# 把node和带权重的edge添加到graph
 graph.add_nodes_from(nodes)
 graph.add_weighted_edges_from(edges)
-
+# position
 pos = nx.spiral_layout(graph)
+# 取weight数据并设置edge label
 edge_label = nx.get_edge_attributes(graph, 'weight')
-
 nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_label)
 nx.draw(graph, with_labels=True, node_color=nodes.values(), pos=pos)
 plt.show()
-
-
-print(graph.edges(data=True))
-print(graph.nodes)
