07a8c15c8fcfd49c37a299a0746606584eaf92bb
Vacant <vacant2333@gmail.com>
2021-11-20 18:13:56 +0800

    graph.py

--- 图/算法/Graph.py
+++ 图/算法/Graph.py
@@ -1,6 +1,6 @@
 # 图,用于实现图类的算法
 
-class Graph:
+class UndirectedGraph:
     # 节点 [str]
     nodes = []
     # 边 [[node_1, node_2]]
@@ -10,35 +10,49 @@ class Graph:
     def insert_node(self, node: str or list):
         if isinstance(node, str):
             # node为str类型
-            if len(node) >= 1:
-                # 检查node是否重复
-                if not self.exists_node(node):
-                    self.nodes.append(node)
-                else:
-                    raise ValueError("node duplication : " + str(node))
-            else:
-                raise ValueError("node length must >= 1")
+            if self.check_node(node):
+                self.nodes.append(node)
         elif isinstance(node, list):
             # node为list类型
             for one_node in node:
-                if isinstance(one_node, str):
-                    # 检查node是否重复
-                    if not self.exists_node(one_node):
-                        self.nodes.append(one_node)
-                    else:
-                        raise ValueError("node duplication : " + str(one_node))
-                else:
-                    raise ValueError("node must be str or list[str]")
+                if self.check_node(one_node):
+                    self.nodes.append(one_node)
         else:
-            # node类型不为str或list
-            raise ValueError("node must be str or list[str]")
+            # node不是str或list
+            raise ValueError("Node must be str or list[str]! node:" + str(node))
 
-    # 检查node是否已存在
-    def exists_node(self, node: str):
-        return node in self.nodes
+    # 插入edge, edge可为list或list[list]
+    def insert_edge(self, edge: list):
+        pass
+
+    # 检查一个node是否符合 长度大于0且是str类型,不重复
+    def check_node(self, node: str):
+        # 检查node类型是否为str
+        if isinstance(node, str):
+            # 检查node长度是否大于0
+            if len(node) > 0:
+                # 检查node是否重复
+                if node not in self.nodes:
+                    return True
+                else:
+                    raise ValueError("Node duplication! node:" + node)
+            else:
+                raise ValueError("Node length must > 0! node:" + node)
+        else:
+            raise ValueError("Node must be str! node:" + str(node))
+
+    # 检查一条边是否符合 类型为list且两个节点都存在
+    def check_edge(self, edge: list):
+        if isinstance(edge, list):
+            if len(edge) == 2:
+                if self.check_node(edge[0]) and self.check_node(edge[1]):
+                    return True
+                else:
+                    raise ValueError("Edge node error" + str(edge))
+            else:
+                raise ValueError("Edge length must be 2" + str(edge))
+        else:
+            raise ValueError("Edge must be list! edge:" + str(edge))
 
 
-g = Graph()
-g.insert_node(['1', '2', '3', ''])
-g.insert_node('1')
-print(g.nodes)
+g = UndirectedGraph()
