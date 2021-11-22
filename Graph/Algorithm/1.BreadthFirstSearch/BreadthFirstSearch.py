1d81b68f4b111f8995282e99750c339385ca400c
Vacant <vacant2333@gmail.com>
2021-11-23 00:26:49 +0800

    UndirectedGraph.py

--- Graph/Algorithm/1.BreadthFirstSearch/BreadthFirstSearch.py
+++ Graph/Algorithm/1.BreadthFirstSearch/BreadthFirstSearch.py
@@ -2,6 +2,7 @@ class BreadthFirstSearch:
     def __init__(self):
         self.nodes = []
         self.edges = []
+        self.check_list = []
 
     def get_node_neighbor(self, node: str) -> list:
         neighbor = []
@@ -10,3 +11,9 @@ class BreadthFirstSearch:
                 edge.remove(node)
                 neighbor.append(edge.pop())
         return neighbor
+
+    def breadth_first_search(self, nodes: list, edges: list, start: str, end: str) -> bool:
+        self.nodes = nodes
+        self.edges = edges
+        self.check_list = [start]
+        pass
