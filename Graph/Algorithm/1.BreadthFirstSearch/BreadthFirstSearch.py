52b33599eff09fdb8547f43a736336721ba7c598
Vacant <vacant2333@gmail.com>
2021-11-24 01:26:16 +0800

    BreadthFirstSearch

--- Graph/Algorithm/1.BreadthFirstSearch/BreadthFirstSearch.py
+++ Graph/Algorithm/1.BreadthFirstSearch/BreadthFirstSearch.py
@@ -3,6 +3,7 @@ class BreadthFirstSearch:
         self.nodes = []
         self.edges = []
         self.check_list = []
+        self.checked_node = []
 
     def get_node_neighbor(self, node: str) -> list:
         neighbor = []
@@ -12,8 +13,26 @@ class BreadthFirstSearch:
                 neighbor.append(edge.pop())
         return neighbor
 
+    def filter_checked_node(self, nodes: list) -> list:
+        for checked_node in self.checked_node:
+            if checked_node in nodes:
+                nodes.remove(checked_node)
+        return nodes
+
     def breadth_first_search(self, nodes: list, edges: list, start: str, end: str) -> bool:
         self.nodes = nodes
         self.edges = edges
         self.check_list = [start]
-        pass
+        for check_node in self.check_list:
+            if check_node != end:
+                # Get Node neighbor and filter node which had checked
+                node_neighbor = self.filter_checked_node(self.get_node_neighbor(check_node))
+                # Add neighbor node to check_list to search all node
+                self.check_list += node_neighbor
+                self.checked_node.append(check_node)
+            else:
+                # Its end node
+                return True
+        # Cant find end node
+        return False
+
