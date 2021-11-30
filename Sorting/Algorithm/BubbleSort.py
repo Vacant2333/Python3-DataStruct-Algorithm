610c9979867db09653e3cf109b7ee6c3ed3c3975
Vacant <vacant2333@gmail.com>
2021-11-30 21:42:10 +0800

    BubbleSort

--- Sorting/Algorithm/BubbleSort.py
+++ Sorting/Algorithm/BubbleSort.py
@@ -1,14 +1,32 @@
-class BubbleSort:
+class BubbleSort1:
     def run(self, ar: list[int]) -> list[int]:
         arr = ar[:]
-        # End flag,it will return if no action in 1 loop
+        # End flag, it will return if no action in 1 loop
         flag = True
         while flag:
             flag = False
             for index, value in enumerate(arr):
                 if index != len(arr) - 1 and arr[index + 1] < arr[index]:
-                    # If the next item smaller than now,exchange them
+                    # If the next item smaller than now, exchange them
                     arr[index], arr[index + 1] = arr[index + 1], arr[index]
-                    # May more actions,continue
+                    # May more actions, continue
+                    flag = True
+        return arr
+
+
+# Better than 1
+class BubbleSort2:
+    def run(self, ar: list[int]) -> list[int]:
+        arr = ar[:]
+        flag = True
+        for now_round in range(1, len(arr)):
+            if flag == False:
+                break
+            # If there is no action, exit the loop in advance
+            flag = False
+            # The last value of each round is the largest
+            for index in range(0, len(arr) - now_round):
+                if arr[index] > arr[index + 1]:
+                    arr[index], arr[index + 1] = arr[index + 1], arr[index]
                     flag = True
         return arr
--- Sorting/BubbleSort_Demo.py
+++ Sorting/BubbleSort_Demo.py
@@ -1,4 +1,5 @@
-from Sorting.Algorithm.BubbleSort import BubbleSort
+from Sorting.Algorithm.BubbleSort import BubbleSort1, BubbleSort2
 
 arr = [5, 1, 2, 4, 2, 10, 23, 5, 12, 8, 21, 9]
-print(BubbleSort().run(arr))
+print(BubbleSort1().run(arr))
+print(BubbleSort2().run(arr))
