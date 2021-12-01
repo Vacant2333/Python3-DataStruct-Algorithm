fda36bc7c7e583a91b576d35cab8ff167aa1d96b
Vacant <vacant2333@gmail.com>
2021-12-01 21:27:18 +0800

    Insert sort

--- Sorting/Algorithm/InsertSort.py
+++ Sorting/Algorithm/InsertSort.py
@@ -4,11 +4,15 @@ class InsertSort:
         for index in range(1, len(ar) + 1):
             if ar[index * -1] < check_value:
                 return len(ar) + (-1 * index) + 1
-        return len(ar) - 1
+        # The check_value is the smallest one, insert it in index 0
+        return 0
 
     def run(self, ar: list[int]) -> list[int]:
         arr = ar[:]
         for index in range(1, len(arr)):
+            # Pop 1 item and insert back
             value = arr.pop(index)
+            # Insert value into the list where a <= value < b
             arr.insert(self.get_smaller_value_index(arr[:index], value), value)
+            print(arr[:index], self.get_smaller_value_index(arr[:index], value), value, arr)
         return arr
